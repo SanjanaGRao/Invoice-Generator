42a0464be3455f288f2776e711f1076198c3ee3e
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-26 14:12:21 +0530

    Added: Ability to add multiple rides and calculate aggregate total for all

--- src/test/java/com/bridgelabzassignment/InvoiceGenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabzassignment/InvoiceGenerator/InvoiceGeneratorTest.java
@@ -24,4 +24,15 @@ public class InvoiceGeneratorTest
 		double fare = invoiceGenerator.calculateTotalFare(distance, time);
 		Assert.assertEquals(5.0, fare, 0.0);
 	}
+	
+	@Test
+	public void givenMultipleRides_ShouldReturnTotalFare()
+	{
+		InvoiceGenerator invoiceGenerator =  new InvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5.0),
+				         new Ride(0.1, 1.0)
+				       };
+		double fare = invoiceGenerator.calculateTotalFare(rides);
+		Assert.assertEquals(30.0, fare, 0.0);
+	}
 }
